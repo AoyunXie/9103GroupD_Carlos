16c7d6c960c9f3dadb0c6b0bc3d1f0fca5d773c5
AoyunXie <axie0331@uni.sydney.edu.au>
2025-06-09 17:06:54 +1000

    add function

--- sketch.js
+++ sketch.js
@@ -12,3 +12,14 @@ function drawLinGroup(x, y, len, count, spacing, weight, alpha) {
     line(x, yOffset, x+len, yOffset)
   } 
 }
+
+function drawTrapezoidLines(x1, x2, x3, x4, y, h, spacing = 3, alpha = 255) {
+  stroke(0, alpha);
+  strokeWeight(1);
+  for (let i = 0; i < h; i++) {
+    let yi = y + i * spacing;
+    let xi1 = lerp(x1, x3, i / h);
+    let xi2 = lerp(x2, x4, i / h);
+    line(xi1, yi, xi2, yi);
+  }
+}
