337c061adbd83b553c1c4ab6431b245754e2c968
AriaXieym <yxie0369@uni.sydney.edu.au>
2025-06-09 17:04:39 +1000

    Update sketch.js
    Change background's colour & function drawLineGroup

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+  background(247,241,225)
 }
 
-function draw() {
-  background(220);
+function drawLinGroup(x, y, len, count, spacing, weight, alpha) {
+  strokeWeight(weight);
+  stroke(0,alpha);
+  
+  for (let i = 0; i < count; i++){
+    let yOffset = y + i * spacing;
+    line(x, yOffset, x+len, yOffset)
+  } 
 }
