8f290ee82a4ad5b23257680f379ccc3e72285f19
AoyunXie <axie0331@uni.sydney.edu.au>
2025-06-10 22:52:57 +1000

    Add ThickGroup5-8

--- sketch.js
+++ sketch.js
@@ -1,7 +1,48 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
   background(247,241,225)
 }
+
+function draw() {
+  // Thick 5
+   push();
+   translate(189, 343);       
+   drawLinGroup(0, 0, 500, 2, 1, 1, 255);  
+   pop();
+ 
+   push();
+   translate(198, 360);
+   drawLinGroup(0, 0, 350, 2, 1, 1, 255);  
+   pop();
+ 
+ // Thick 6
+   push();
+   translate(424, 430);     
+   drawLinGroup(0, 0, 150, 2, 1, 1, 255); 
+   pop();
+   
+   push();
+   translate(419, 436);     
+   drawLinGroup(0, 0, 155, 2, 1, 1, 255); 
+   pop();
+ 
+ // Thick 7
+   push();
+   translate(130, 550);     
+   drawLinGroup(0, 0, 600, 2, 1, 1, 255); 
+   pop();
+ 
+ // Thick 8
+   push();
+   translate(423, 450);     
+   drawLinGroup(0, 0, 155, 4, 1, 1, 255); 
+   pop();
+ 
+   push();
+   translate(131, 489);     
+   drawLinGroup(0, 0, 100, 3, 1, 1, 255); 
+   pop();
+ }
 
 function drawLinGroup(x, y, len, count, spacing, weight, alpha) {
   strokeWeight(weight);
